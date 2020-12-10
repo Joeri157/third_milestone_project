747b351b84d43e8b64efe3a626a15237d3aa86ad
Joeri157 <joeri_vanden_kieboom@hotmail.com>
2020-12-10 15:03:34 +0000

    Update login.html version 1.0
    @login.html
    Added login form
    @style.css
    modified classnames to match with the register.html ones
    @app.py
    Added login functionality

--- app.py
+++ app.py
@@ -74,6 +74,29 @@ def register():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    # check if username exists in db
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one({
+            "username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome, {}".format(
+                        request.form.get("username")))
+                    return redirect(url_for(
+                        "profile", username=["user"]))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
     return render_template("login.html")
 
 
