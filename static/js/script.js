4f9c506eb17cfe0d58fd9dba79388828c9291363
Joeri157 <joeri_vanden_kieboom@hotmail.com>
2020-12-21 13:13:54 +0000

    Update Edit Upload version 1.5
    @edit_upload.html
    Added a Preview image when image is selected
    
    @style.scss/style.css
    Added upload image button (modified it) from - https://codepen.io/stebaker/pen/tImBc -
    Added styling to the preview_image class in edit_upload
    
    @script.js
    Added text input value when file is being uploaded
    Added a preview url for image when file is selected

--- static/js/script.js
+++ static/js/script.js
@@ -48,5 +48,16 @@ $(document).ready(function(){
         coverTrigger: false,
         alignment: "right"
         });
+    
+    // upload text
+    $('input[type="file"]').change(function(){
+        var value = $("input[type='file']").val();
+        $('.js-value').text(value);
+    });
+
+    $('input[type="file"]').change( function(event) {
+        var tmppath = URL.createObjectURL(event.target.files[0]);
+        $("img").fadeIn("fast").attr('src',URL.createObjectURL(event.target.files[0]));
+    });
 
 });
